d09a03e545dcd27f025295782b0fad6871b6693a
Alisa Kuraieva <alisa.mogila@gmail.com>
2025-04-08 21:16:46 +0300

    commit

--- global.styles.js
+++ global.styles.js
@@ -3,13 +3,6 @@ const breakpointsStyles = '@import "./src/scss/common/_breakpoints.scss"';
 const mixinsStyles = '@import "./src/scss/common/_mixins.scss"';
 const resetStyles = '@import "./src/scss/common/_reset.scss"';
 
-const styles = [
-  variablesStyles,
-  breakpointsStyles,
-  //   colorsStyles,
-  mixinsStyles,
-  //   baseStyles,
-  resetStyles,
-];
+const styles = [variablesStyles, breakpointsStyles, mixinsStyles, resetStyles];
 
 export const globalStylesOptions = styles.reduce((acc, i) => acc + i + ';', '');
